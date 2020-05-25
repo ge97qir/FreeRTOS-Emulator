48f22436ba390cbc3e5282d4846189fc23945dd7
Luka Keseric <ge97qir@mytum.de>
2020-05-26 00:30:01 +0200

    implemented the state machine for 3 states

--- src/main.c
+++ src/main.c
@@ -23,12 +23,36 @@
 #define mainGENERIC_PRIORITY (tskIDLE_PRIORITY)
 #define mainGENERIC_STACK_SIZE ((unsigned short)2560)
 #define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
+
 #define SHAPE_SIZE 38
 #define RIGHT 1
 #define LEFT 0
 #define PRESSED 1
 
-static TaskHandle_t exercise2 = NULL;
+#define EX3_FONT "IBMPlexSans-SemiBold.ttf"
+#define EX4_FONT "IBMPlexSans-BoldItalic.ttf"
+
+#define STATE_QUEUE_LENGTH 1
+#define STATE_COUNT 3
+#define STATE_ONE 0
+#define STATE_TWO 1
+#define STATE_THREE 2
+
+#define NEXT_TASK 0
+#define PREV_TASK 1
+
+#define STARTING_STATE STATE_ONE
+#define STATE_DEBOUNCE_DELAY 500
+
+const unsigned char next_state_signal = NEXT_TASK;
+const unsigned char prev_state_signal = PREV_TASK;
+
+static TaskHandle_t StateMachine = NULL;
+static TaskHandle_t Exercise2 = NULL;
+static TaskHandle_t Exercise3 = NULL;
+static TaskHandle_t Exercise4 = NULL;
+
+static QueueHandle_t StateQueue = NULL;
 
 typedef struct buttons_buffer {
     unsigned char buttons[SDL_NUM_SCANCODES];
@@ -163,7 +187,7 @@ void vDrawButtonText(unsigned int *buttonInput)
 	    pressedA += (*buttonInput++);
         pressedB += (*buttonInput++);
         pressedC += (*buttonInput++);
-        pressedD += (*buttonInput++);
+        pressedD += (*buttonInput);
 
         sprintf(str, "A: %3d | B: %3d | C: %3d | D: %3d",
                 pressedA, pressedB, pressedC, pressedD);
@@ -211,12 +235,261 @@ unsigned int* vDebounceButtons(){
     return reading;
 }
 
+static int vCheckStateInput(void)
+{
+    if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+        if (buttons.buttons[KEYCODE(C)]) {
+            buttons.buttons[KEYCODE(C)] = 0;
+            if (StateQueue) {
+                xSemaphoreGive(buttons.lock);
+                xQueueSend(StateQueue, &next_state_signal, 0);
+                return -1;
+            }
+        }
+        xSemaphoreGive(buttons.lock);
+    }
+
+    return 0;
+}
+
+void changeState(volatile unsigned char *state, unsigned char forwards)
+{
+    switch (forwards) {
+        case NEXT_TASK:
+            if (*state == STATE_COUNT - 1) {
+                *state = 0;
+            }
+            else {
+                (*state)++;
+            }
+            break;
+        case PREV_TASK:
+            if (*state == 0) {
+                *state = STATE_COUNT - 1;
+            }
+            else {
+                (*state)--;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+/*
+ * Example basic state machine with sequential states
+ */
+void basicSequentialStateMachine(void *pvParameters)
+{
+    unsigned char current_state = STARTING_STATE; // Default state
+    unsigned char state_changed =
+        1; // Only re-evaluate state if it has changed
+    unsigned char input = 0;
+
+    const int state_change_period = STATE_DEBOUNCE_DELAY;
+
+    TickType_t last_change = xTaskGetTickCount();
+
+    while (1) {
+        if (state_changed) {
+            goto initial_state;
+        }
+
+        // Handle state machine input
+        if (StateQueue)
+            if (xQueueReceive(StateQueue, &input, portMAX_DELAY) ==
+                pdTRUE)
+                if (xTaskGetTickCount() - last_change >
+                    state_change_period) {
+                    changeState(&current_state, input);
+                    state_changed = 1;
+                    last_change = xTaskGetTickCount();
+                }
+
+initial_state:
+        // Handle current state
+        if (state_changed) {
+            switch (current_state) {
+                case STATE_ONE:
+                    if (Exercise4) {
+                        vTaskSuspend(Exercise4);
+                    }
+                    if (Exercise3) {
+                        vTaskSuspend(Exercise3);
+                    }
+                    if (Exercise2) {
+                        vTaskResume(Exercise2);
+                    }
+                    break;
+                case STATE_TWO:
+                    if (Exercise4) {
+                        vTaskSuspend(Exercise4);
+                    }
+                    if (Exercise2) {
+                        vTaskSuspend(Exercise2);
+                    }
+                    if (Exercise3) {
+                        vTaskResume(Exercise3);
+                    }
+                    break;
+                case STATE_THREE:
+                    if (Exercise3) {
+                        vTaskSuspend(Exercise3);
+                    }
+                    if (Exercise2) {
+                        vTaskSuspend(Exercise2);
+                    }
+                    if (Exercise4) {
+                        vTaskResume(Exercise4);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            state_changed = 0;
+        }
+    }
+}
+
+void vExercise4(void *pvParameters){
+
+    static char quitInstructions[100];
+    static int quitInstructionssWidth = 0;
+    
+    static char randomString[100];
+    static int randomStringWidth = 0;
+    static int randomStringHeight = 0;
+    
+    // Format our string into our char array
+    sprintf(quitInstructions,"Press Q to quit or C to change state.");
+    sprintf(randomString, "Exercise 4?"); 
+    
+    tumDrawBindThread();
+
+    while (1) {
+        // Query events backend for new events, ie. button presses
+        tumEventFetchEvents();    
+   
+        xGetButtonInput(); // Update global input
+        
+        if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+            if (buttons.buttons[KEYCODE(Q)]) {
+                exit(EXIT_SUCCESS);
+            }
+            xSemaphoreGive(buttons.lock);
+        }
+
+        tumDrawClear(TUMBlue); // Clear screen
+ 
+        // Get the width of the string on the screen so we can center it
+        // Returns 0 if width was successfully obtained
+        if (!tumGetTextSize((char *)quitInstructions,
+                            &quitInstructionssWidth, NULL)){
+            tumDrawText(quitInstructions,
+                        SCREEN_WIDTH / 2 - quitInstructionssWidth / 2
+                        + moveWithMouseInX(),
+                        SCREEN_HEIGHT - 5 * DEFAULT_FONT_SIZE
+                        + moveWithMouseInY(),
+                        White);
+	    }
+        // saves current font
+        font_handle_t cur_font = tumFontGetCurFontHandle();
+        // loads new font
+        tumFontSelectFontFromName(EX4_FONT);
+       
+        // Draws the random text
+        if(!tumGetTextSize((char *)randomString, 
+                           &randomStringWidth, &randomStringHeight)){
+            // draws the string
+            tumDrawText(randomString, 
+                       SCREEN_WIDTH / 2 - randomStringWidth / 2
+                       + moveWithMouseInX(),
+                       SCREEN_HEIGHT / 2 - randomStringHeight 
+                       + moveWithMouseInY(), 
+                       Pink);
+        }
+        
+        // loads the previously saved font
+        tumFontSelectFontFromHandle(cur_font);
+        // frees the saved font from memory
+        tumFontPutFontHandle(cur_font);
+
+        tumDrawUpdateScreen(); // Refresh the screen to draw everything 
+
+	    vCheckStateInput(); 
+    }
+}
+
+
+void vExercise3(void *pvParameters){
+
+    static char quitInstructions[100];
+    static int quitInstructionssWidth = 0;
+    
+    static char randomString[100];
+    static int randomStringWidth = 0;
+    
+    // Format our string into our char array
+    sprintf(quitInstructions,"Press Q to quit or C to change state.");
+    sprintf(randomString, "Exercise 3!"); 
+    
+    tumDrawBindThread();
+
+    while (1) {
+        // Query events backend for new events, ie. button presses
+        tumEventFetchEvents();    
+   
+        xGetButtonInput(); // Update global input
+        
+        if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
+            if (buttons.buttons[KEYCODE(Q)]) {
+                exit(EXIT_SUCCESS);
+            }
+            xSemaphoreGive(buttons.lock);
+        }
+
+        tumDrawClear(White); // Clear screen
+ 
+        // Get the width of the string on the screen so we can center it
+        // Returns 0 if width was successfully obtained
+        if (!tumGetTextSize((char *)quitInstructions,
+                            &quitInstructionssWidth, NULL)){
+            tumDrawText(quitInstructions,
+                        SCREEN_WIDTH / 2 - quitInstructionssWidth / 2,
+                        SCREEN_HEIGHT - 2 * DEFAULT_FONT_SIZE,
+                        TUMBlue);
+	    }
+        // saves current font
+        font_handle_t cur_font = tumFontGetCurFontHandle();
+        // loads new font
+        tumFontSelectFontFromName(EX3_FONT);
+       
+        // Draws the random text
+        if(!tumGetTextSize((char *)randomString, &randomStringWidth, NULL)){
+             // draws the string
+            tumDrawText(randomString, 
+                       SCREEN_WIDTH / 2 - randomStringWidth / 2,
+                       DEFAULT_FONT_SIZE, 
+                       TUMBlue);
+        }
+        
+        // loads the previously saved font
+        tumFontSelectFontFromHandle(cur_font);
+        // frees the saved font from memory
+        tumFontPutFontHandle(cur_font);
+
+        tumDrawUpdateScreen(); // Refresh the screen to draw everything 
+
+	    vCheckStateInput();
+    }
+
+}
+
 void vExercise2(void *pvParameters)
 {
     TickType_t delay = 10;
     unsigned int *pressedButtons;
 
-    // structure to store time retrieved from Linux kernel
     static char quitInstructions[100];
     static int quitInstructionssWidth = 0;
     
@@ -226,7 +499,7 @@ void vExercise2(void *pvParameters)
     createMovingStr(movingStringPosition);
 
     // Format our string into our char array
-    sprintf(quitInstructions,"Press Q to quit.");
+    sprintf(quitInstructions,"Press Q to quit or C to change state.");
     sprintf(movingString, 
             "Click right mouse button to resset the pressed button count."); 
     
@@ -306,6 +579,8 @@ void vExercise2(void *pvParameters)
 
         tumDrawUpdateScreen(); // Refresh the screen to draw everything 
 
+        vCheckStateInput();
+
 	    vTaskDelay(delay);
    }
 }
@@ -331,22 +606,58 @@ int main(int argc, char *argv[])
         goto err_init_audio;
     }
 
+    tumFontLoadFont(EX4_FONT, 5 * DEFAULT_FONT_SIZE);
+
     buttons.lock = xSemaphoreCreateMutex(); // Locking mechanism
     if (!buttons.lock) {
         PRINT_ERROR("Failed to create buttons lock");
         goto err_buttons_lock;
     }
-
-    if (xTaskCreate(vExercise2, "Exercise 2", mainGENERIC_STACK_SIZE * 2,
-		    NULL, mainGENERIC_PRIORITY, &exercise2) != pdPASS) {
-        goto err_exercise2;
+    
+    StateQueue = xQueueCreate(STATE_QUEUE_LENGTH, sizeof(unsigned char));
+    if (!StateQueue) {
+        PRINT_ERROR("Could not open state queue");
+        goto err_state_queue;
+    }
+    
+    if (xTaskCreate(basicSequentialStateMachine, "StateMachine",
+                    mainGENERIC_STACK_SIZE * 2, NULL,
+                    configMAX_PRIORITIES - 1, StateMachine) != pdPASS) {
+        goto err_statemachine;
     }
 
+    if (xTaskCreate(vExercise2, "Exercise 2", mainGENERIC_STACK_SIZE * 2,
+		    NULL, mainGENERIC_PRIORITY, &Exercise2) != pdPASS) {
+        goto err_exercise2;
+    }
+    
+    if (xTaskCreate(vExercise3, "Exercise 3", mainGENERIC_STACK_SIZE * 2,
+		    NULL, mainGENERIC_PRIORITY, &Exercise3) != pdPASS) {
+        goto err_exercise3;
+    }
+
+    if (xTaskCreate(vExercise4, "Exercise 4", mainGENERIC_STACK_SIZE * 2,
+		    NULL, mainGENERIC_PRIORITY, &Exercise4) != pdPASS) {
+        goto err_exercise4;
+    }
+
+    vTaskSuspend(Exercise2);
+    vTaskSuspend(Exercise3);
+    vTaskSuspend(Exercise4);    
+    
     vTaskStartScheduler();
 
     return EXIT_SUCCESS;
 
+err_exercise4:
+    vTaskDelete(Exercise3);
+err_exercise3:
+    vTaskDelete(Exercise2);
 err_exercise2:
+    vTaskDelete(StateMachine);
+err_statemachine:
+    vQueueDelete(StateQueue);
+err_state_queue:
     vSemaphoreDelete(buttons.lock);
 err_buttons_lock:
     tumSoundExit();
